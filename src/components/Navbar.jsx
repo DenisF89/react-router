b4e22429ee1aaf45fbc9354a325d1493ce8230aa
Denis89 <filippinidenis@gmail.com>
2026-02-06 19:52:50 +0100

    aggiunto useLocation per visualizzare BudgetMode solo su pagina Prodotti (non gestito su SingleProduct)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { NavLink } from "react-router-dom";
+import { NavLink,useLocation  } from "react-router-dom";
 import { useBudget } from '../contexts/BudgetContext';
+
 
 //La Navbar gestisce i link del menu principale con NavLink di React Router
 //se uno dei link corrisponde alla pagina corrente, gli viene dato classe active
@@ -7,6 +8,8 @@ import { useBudget } from '../contexts/BudgetContext';
 export default function Navbar(){
 
     const [budgetMode, setBudgetMode] = useBudget();
+    
+    const location = useLocation();
 
     const myBudgetMode = ()=>{
         setBudgetMode(bm=>!bm)
@@ -19,10 +22,16 @@ export default function Navbar(){
                 <NavLink className="button" to="/chi-siamo">Chi Siamo</NavLink>
                 <NavLink className="button" to="/prodotti">Prodotti</NavLink>
             </nav>
-            <button className="button" name="Modalità Budget" 
-                    onClick={myBudgetMode}>
-                        {budgetMode?"Disattiva":"Attiva"} Modalità Budget
-            </button>
-        </div>
+
+           
+            {
+                location.pathname === "/prodotti" && (
+                    <button className="button" name="Modalità Budget" onClick={myBudgetMode}>
+                    {budgetMode ? "Disattiva" : "Attiva"} Modalità Budget
+                    </button>
+                    )
+            }
+           
+    </div>
     );
 }

e6dbecd9a8638636f83ff6926075896090b43299
Denis89 <filippinidenis@gmail.com>
2026-02-13 16:32:51 +0100

    Modale per eliminazione prodotto dal carrello

--- src/pages/Carrello.jsx
+++ src/pages/Carrello.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import {useShop} from "../contexts/ShopContext";
 
 function Carrello(){
@@ -8,6 +9,18 @@ function Carrello(){
         setCart(cart => ({...cart, [id]: Number(e.target.value)}))
     }
 
+    const [isVisible,setIsVisible] = useState(false);
+    const [toDelete,setToDelete] = useState({id:0, tit:"", qta:0});
+
+    const openModal = (i,num,title)=>{
+        setIsVisible(true);
+        setToDelete(()=>({id:Number(i), tit:title, qta:Number(num)}));
+    }
+    const hideModal = ()=>{
+        setIsVisible(false);
+        setToDelete(()=>({id:0, tit:"", qta:0}));
+    }
+
     const deleteItem = (id) =>{
          /* setCart(cart => {
                 const newCart = { ...cart };
@@ -15,9 +28,13 @@ function Carrello(){
                 return newCart;
         }); */
          setCart(({ [id]: _, ...cart }) => cart);
+         hideModal();
     }
 
+    
+
     const isEmpty = (Object.keys(cart).length === 0);
+    
 
     const total = Object.entries(cart).reduce((acc, [id, quantity]) => {
             const product = products.find(p => p.id === Number(id));
@@ -25,42 +42,21 @@ function Carrello(){
             return acc + Number(product.price) * Number(quantity);
             }, 0);
 
+
     return(
-        <div>
-            <h1>Carrello</h1>
-            <div className="flex">
+            
             <div className="cart-container">
+                <h1>Carrello</h1>
             {isEmpty    ?<p>Il carrello è vuoto</p>
-                        :Object.entries(cart).map(([id, num]) => {
-            const product = products.find(p => p.id === Number(id));
-            if (!product) return null;
-            return(
-                <div key={id} className="cart-product">
-                    <div className="cart-image">
-                        <img src={product.image} alt={product.title}  />
-                    </div>     
-                    <div className="cart-info">
-                        <h2>{product.title}</h2>
-                        {product.price &&
-                        <p>Prezzo: {Number(product.price).toFixed(2)} €</p> }
-                        <input className="count" name="count" type="number" min="1" max="99" value={num} onChange={(e)=>changeNum(e,id)} />
-                        <br /><br />
-                        <p>Totale: {(Number(product.price)*Number(num)).toFixed(2)} €</p>
-                    </div> 
-                    <button className="cart-delete" onClick={(e)=>deleteItem(id)} >X</button>
-                </div>
-                
-            )  
-            })}   
-            </div>   
+                        :
             <div className="cart-total">
-                <h2>Totale</h2>
+                
                 <table>
                     <thead>
                         <tr>
-                            <th>Art.</th>
-                            <th>Quantità</th>
+                            <th colSpan="3">Articolo</th>
                             <th>Prezzo</th>
+                            <th>Quantità</th>
                             <th>Totale riga</th>
                         </tr>
                     </thead>
@@ -68,26 +64,54 @@ function Carrello(){
                         const product = products.find(p => p.id === Number(id));
                         return(
                         <tr key={id}>
-                        <td>{id}</td>
-                        <td>{num}</td>
+                        <td><button className="cart-delete" onClick={(e)=>openModal(id,num,product.title)}>X</button></td>
+                        <td><div className="cart-image">
+                                <img src={product.image} alt={product.title}  />
+                            </div>
+                        </td>
+                        <td>{product.title}</td>
                         <td>{Number(product.price).toFixed(2)} €</td>
+                        <td><input className="count" name="count" type="number" min="1" max="99" value={num} onChange={(e)=>changeNum(e,id)} /></td>
                         <td>{(Number(product.price)*Number(num)).toFixed(2)} €</td>
                         </tr>
                      )})
                     }</tbody>
                     <tfoot>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td colSpan="4"></td>
+                            <td>Totale</td>
                             <td>{total.toFixed(2)} €</td>
                         </tr>
                     </tfoot>
                 </table>
-               
             </div>
-            </div>
+            }
+                            
+    {/* MODALE CONFERMA ELIMINAZIONE */}
+                
+            {
+                isVisible && toDelete.id &&
+
+                <div className="overlay">
+
+                    <div className="modal">
+
+                        <h3 className="m-title">Conferma eliminazione</h3>
+
+                        <p className="m-text">Stai per eliminare {toDelete.qta} {toDelete.tit} dal carrello.</p>
+
+                        <p className="m-text">Sei sicuro di voler continuare?</p>
+
+                        <div className="m-buttons">
+                            <button className="m-btn m-cancel" onClick={hideModal}>Annulla</button>
+                            <button className="m-btn m-delete" onClick={(e)=>deleteItem(toDelete.id)}>Elimina</button>
+                        </div>
+                    </div>
+                </div>
+            }
+
         </div>
+            
     )
 }
 
